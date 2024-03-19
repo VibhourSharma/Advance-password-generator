aef06cdeae2a8b1039e0fb6343d556c2704fd056
Vibhour Sharma <vibhour121@gmail.com>
2024-03-19 14:10:16 +0530

    🚀Feat: Result and Strength field updated

--- src/ResultField.jsx
+++ src/ResultField.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { usePassword } from "./context/Password";
 
 const ResultField = () => {
+  const [copied, setCopied] = useState(false);
   const { generatedPassword } = usePassword();
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(generatedPassword);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
     <div className="result-field-main">
-      <div placeholder="P4$W0rD!" className="password-field" readOnly>
-        {generatedPassword}
+      <div className="password-field" onClick={handleCopy}>
+        {generatedPassword ? (
+          generatedPassword
+        ) : (
+          <span style={{ color: "#807c92" }}>P4$W0rD!</span>
+        )}
       </div>
-      <div>
-        <span>COPIED!</span>
-        <button className="copy-button">
+      <div className="popup-container">
+        <span style={{ color: "#a3ffae" }}>{copied ? "Copied!" : ""}</span>
+        <button className="copy-button" onClick={handleCopy}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
+            strokeLinecap="round"
             stroke="currentColor"
             className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
             />
           </svg>
--- src/StrengthField.jsx
+++ src/StrengthField.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { usePassword } from "./context/Password";
 
-const StrengthField = ({ generatedPassword }) => {
-  return <div className="strength-field">strength</div>;
+const StrengthField = () => {
+  const { generatedPassword } = usePassword();
+
+  const passwordStrength = () => {
+    if (generatedPassword.length === 0) {
+      return "Generate Password";
+    } else if (generatedPassword.length <= 5) {
+      return "Too weak";
+    } else if (generatedPassword.length <= 9) {
+      return "medium";
+    } else if (generatedPassword.length <= 14) {
+      return "strong";
+    } else {
+      return "too-strong";
+    }
+  };
+
+  const strengthClass = passwordStrength();
+
+  return (
+    <div className="strength-field">
+      <span>Strength</span>
+      <span className={`password-strength ${strengthClass}`}>
+        {passwordStrength()}
+      </span>
+    </div>
+  );
 };
 
 export default StrengthField;
